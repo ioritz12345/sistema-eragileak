c5192c067290d272d9bfc19fbad279adc49ec749
ioritz12345 <157359012+ioritz12345@users.noreply.github.com>
2024-04-25 08:44:38 +0400

    scriptak

---
+++ SCRIPT/ariekta2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "sartu urte bat"
+
+read URTEA
+
+echo "sartu hilabete bat"
+
+read HILABETEA
+
+cal $HILABETEA $URTEA
---
+++ SCRIPT/ariketa1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1 Ariekta
+
+Clear
+
+echo "Nor zara"
+whoami
+
+echo "Zein direktoriotan zaude"
+pwd
